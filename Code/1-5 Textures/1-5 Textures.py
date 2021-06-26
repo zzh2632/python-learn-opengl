bc213957878ca71e115b422ca78060a8b1d3435f
zzh2632 <30931947+zzh2632@users.noreply.github.com>
2021-06-26 22:39:05 +0800

    Fix: indices use np.uint32

--- Code/1-5 Textures/1-5 Textures.py
+++ Code/1-5 Textures/1-5 Textures.py
@@ -33,16 +33,17 @@ def main():
     # 定义顶点数据
     vertices = [
         # ---- 位置 - ---       ---- 颜色 - ---     - 纹理坐标 -
-        0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,  # 右上
-        0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,  # 右下
-        - 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,  # 左下
+        0.5, 0.5, 0.0,      1.0, 0.0, 0.0,  1.0, 1.0,  # 右上
+        0.5, -0.5, 0.0,     0.0, 1.0, 0.0,  1.0, 0.0,  # 右下
+        - 0.5, -0.5, 0.0,   0.0, 0.0, 1.0,  0.0, 0.0,  # 左下
+        -0.5, 0.5, 0.0,     1.0, 1.0, 0.0,  0.0, 1.0   # 左上
     ]
     vertices = np.array(vertices, dtype=np.float32)
     indices = [
         0, 1, 3,  # first triangle
         1, 2, 3  # second triangle
     ]
-    indices = np.array(indices, dtype=np.float32)
+    indices = np.array(indices, dtype=np.uint32)
 
     # 在GPU上创建内存用于储存顶点数据，配置OpenGL如何解释这些内存，并且指定其如何发送给显卡，交给顶点着色器处理
     VAO = glGenVertexArrays(1)
@@ -78,21 +79,22 @@ def main():
     # -------------------------------------------------------------------
     # 链接顶点属性
     # 我们必须手动指定输入数据的哪一个部分对应顶点着色器的哪一个顶点属性
+    # GL_FLOAT长度为4字节
     # -------------------------------------------------------------------
     # position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(0))
     glEnableVertexAttribArray(0)
     # color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(12))
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(3*4))
     glEnableVertexAttribArray(1)
     # texturecoord attribute
     glEnableVertexAttribArray(2)
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(24))
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(6*4))
 
     # -------------------------------------------------------------------
     # 纹理
     # -------------------------------------------------------------------
-    texture = glGenTextures(1)  # 创建纹理
+    texture = glGenTextures(1)  # 创建1个纹理
     glBindTexture(GL_TEXTURE_2D, texture)  # 绑定创建的纹理
 
     # 为当前绑定的纹理对象设置环绕、过滤方式
@@ -103,11 +105,12 @@ def main():
 
     # 加载并生成纹理
     # image = Image.open("wall.jpg")
-    image = Image.open("textures/cat.png")
+    # image = Image.open("textures/cat.png")
+    image = Image.open("textures/container.jpg")
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    img_data = image.convert("RGBA").tobytes()
+    data = image.convert("RGBA").tobytes()
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
     glGenerateMipmap(GL_TEXTURE_2D)
 
     # -------------------------------------------------------------------
@@ -127,17 +130,16 @@ def main():
         glBindTexture(GL_TEXTURE_2D, texture)
 
         # 绘制
-        glDrawArrays(GL_TRIANGLES, 0, 3)
-
-        # glBindVertexArray(VAO)
-        # glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+        glBindVertexArray(VAO)
+        # glDrawArrays(GL_TRIANGLES, 0, 4)
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
 
         glfw.swap_buffers(window)  # 交换颜色缓冲
         glfw.poll_events()  # 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数
 
-    glDeleteVertexArrays(1, VAO)
-    glDeleteBuffers(1, VBO)
-    glDeleteBuffers(1, EBO)
+    # glDeleteVertexArrays(1, VAO)
+    # glDeleteBuffers(1, VBO)
+    # glDeleteBuffers(1, EBO)
 
     # 渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
     glfw.terminate()
