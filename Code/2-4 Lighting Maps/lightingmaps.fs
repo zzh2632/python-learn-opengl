1d1fef103abe13bb980e524008d503722c82abda
zzh2632 <30931947+zzh2632@users.noreply.github.com>
2021-07-04 13:32:32 +0800

    Commit 2-4 Lighting Maps

---
+++ Code/2-4 Lighting Maps/lightingmaps.fs
@@ -0,0 +1,49 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoord;
+
+uniform vec3 objectColor;
+uniform vec3 viewPos;
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+uniform Material material;
+
+
+void main()
+{
+    // ambient
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoord).rgb;
+
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * light.diffuse * texture(material.diffuse, TexCoord).rgb;
+
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = texture(material.specular, TexCoord).rgb * spec * light.specular;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
---
+++ Code/2-4 Lighting Maps/lightingmaps.vs
@@ -0,0 +1,21 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 Normal;
+out vec3 FragPos;
+out vec2 TexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    // 获取世界空间下的片段位置
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = aNormal;
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    TexCoord = aTexCoord;
+}
